925569f36c2eae787edea51c72d6fc5e3925ab88
Robert Kirkman <robert.kirkman@ttu.edu>
2022-12-17 03:01:11 -0600

    update SDL2 to 2.26.1 and move C portion to submodule - fixes Android 12+ and external gamepads

--- getSDL.sh
+++ getSDL.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-
-pushd app/jni/SDL
-
-wget https://www.libsdl.org/release/SDL2-2.0.12.zip
-unzip -q SDL2-2.0.12.zip
-mv SDL2-2.0.12/src src
-mv SDL2-2.0.12/include include
-
+pushd app/jni/SDL/include
 #MSYS2's symlinks seem to just copy the folder which isn't really a good thing recursively. Native symlinks are broken for me so let's just use Windows's implementation
 if [[ -n "$WINDIR" ]]; then
-    cmd <<< "mklink /D include\SDL2 ."
+    cmd <<< "mklink /D SDL2 ."
 else
-    ln -s . include/SDL2
+    ln -s . SDL2
 fi
-
-rm SDL2-2.0.12.zip
-rm -r SDL2-2.0.12
-
 popd
