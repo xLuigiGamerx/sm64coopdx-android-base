67bb07f389107bd6529c5ffae3981873f060eb52
Robert Kirkman <robert.kirkman@ttu.edu>
2023-01-13 03:26:26 -0600

    use /storage/emulated/0 if granted permission

--- app/src/main/java/com/owokitty/sm64excoop/sm64excoopActivity.java
+++ app/src/main/java/com/owokitty/sm64excoop/sm64excoopActivity.java
@@ -1,11 +1,19 @@
 package com.owokitty.sm64excoop;
 
+import android.app.AlertDialog;
+import android.text.InputType;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
 import org.libsdl.app.SDLActivity;
+
+import java.util.Objects;
 
 public class sm64excoopActivity extends SDLActivity
 {
-    // @Override
-    // protected String getMainFunction() {
-    //     return "main";
-    // }
+
 }
