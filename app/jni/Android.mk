39a1bbca34fe564bf3f723730c714daf7a7ab245
Robert Kirkman <robert.kirkman@ttu.edu>
2023-04-30 00:06:15 -0500

    deduplicate liblua source from the gradle project repository

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -1,1 +1,7 @@
-include $(call all-subdir-makefiles)
+MY_LOCAL_PATH := $(call my-dir)
+LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(LOCAL_PATH)/SDL/Android.mk
+LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(LOCAL_PATH)/src/lib/src/lua/Android.mk
+LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(LOCAL_PATH)/src/Android.mk
