85a4e90ffa7a23ca792f9ac8670736411c3e1e3a
Robert Kirkman <robert.kirkman@ttu.edu>
2023-04-30 04:00:34 -0500

    gradle+ndk-build ready for beta 34

--- app/jni/Android.mk
+++ app/jni/Android.mk
@@ -4,4 +4,8 @@ include $(LOCAL_PATH)/SDL/Android.mk
 LOCAL_PATH := $(MY_LOCAL_PATH)
 include $(LOCAL_PATH)/src/lib/src/lua/Android.mk
 LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(LOCAL_PATH)/src/lib/src/coopnet/lib/libjuice/Android.mk
+LOCAL_PATH := $(MY_LOCAL_PATH)
+include $(LOCAL_PATH)/src/lib/src/coopnet/Android.mk
+LOCAL_PATH := $(MY_LOCAL_PATH)
 include $(LOCAL_PATH)/src/Android.mk
