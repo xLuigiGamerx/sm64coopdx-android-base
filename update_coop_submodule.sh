c8d6b7000d306398dc78db39ec3a211a638a5553
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2024-12-01 01:02:31 +0300

    Updated SM64CoopDX submodule to latest commit

--- update_coop_submodule.sh
+++ update_coop_submodule.sh
@@ -3,10 +3,12 @@
 # Fetch and update all submodules to the latest commit on their respective branches
 git submodule update --remote app/jni/src
 
-# Iterate over each submodule and ensure it's on the correct branch
-git submodule foreach 'git fetch && git merge origin/$(git symbolic-ref --short HEAD)'
+# Update only the sm64coopdx submodule
+cd app/jni/src
+git fetch && git merge origin/$(git symbolic-ref --short HEAD)
+cd ../../..
 
 # Commit and push the updates to the main repository
 git add .
-git commit -m "Updated SM64CoopDX submodule to latest commits"
+git commit -m "Updated SM64CoopDX submodule to latest commit"
 git push
