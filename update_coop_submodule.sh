164a66ee08e850b4cbe619a65fa842c0be7d0734
xLuigiGamerx <88401287+xLuigiGamerx@users.noreply.github.com>
2025-06-27 18:13:47 +0300

    Updated the version name and code to match pc coopdx

--- update_coop_submodule.sh
+++ update_coop_submodule.sh
@@ -5,8 +5,3 @@ git submodule update --remote app/jni/src
 
 # Iterate over each submodule and ensure it's on the correct branch
 git submodule foreach 'git fetch && git merge origin/$(git symbolic-ref --short HEAD)'
-
-# Commit and push the updates to the main repository
-git add .
-git commit -m "Updated SM64CoopDX submodule to latest commit"
-git push
